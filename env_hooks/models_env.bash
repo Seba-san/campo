cd76ab866e638ed36311a68002bbaaf8eace3e29
Seba <sansoni.sebas@gmail.com>
2021-02-13 18:42:24 -0300

    intente agregarle el path de gazebo directamente en el CMakeList, pero no resulto como lo esperaba. En el archivo estan las fuentes utilizadas. Al final las seteo pseudo automaticamente desde el 'inicio.sh'

---
+++ env_hooks/models_env.bash
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Entrando a la configuracion buscada 2"
+echo $PWD
+echo "Eso no esta probado, veeer!"
+export Seba=123
+_CATKIN_ENV_HOOKS_PATH = $ (catkin locate arr_gazebo_models)
+#export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:<path to this repo>/models
